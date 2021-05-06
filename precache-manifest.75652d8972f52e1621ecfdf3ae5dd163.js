43b90847a5b3347eda44996f03af606c5195aa16
janjib <janjib04@gmail.com>
2021-05-07 00:27:27 +0800

    Updates

---
+++ precache-manifest.75652d8972f52e1621ecfdf3ae5dd163.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5fc148ad2f5495b1f066a8a0e4e553",
+    "url": "/firegram-tutorial/index.html"
+  },
+  {
+    "revision": "273ce2f14403971b82e6",
+    "url": "/firegram-tutorial/static/css/main.69b19522.chunk.css"
+  },
+  {
+    "revision": "2acccf205b79dd7f9927",
+    "url": "/firegram-tutorial/static/js/2.83f2b0ad.chunk.js"
+  },
+  {
+    "revision": "f13b8ee62297cdeb55e53ad5d3f0bd7f",
+    "url": "/firegram-tutorial/static/js/2.83f2b0ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "273ce2f14403971b82e6",
+    "url": "/firegram-tutorial/static/js/main.c6041c7c.chunk.js"
+  },
+  {
+    "revision": "a9f360c61c913944ab55",
+    "url": "/firegram-tutorial/static/js/runtime-main.bfa07b68.js"
+  }
+]);
